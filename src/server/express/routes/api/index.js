3cd654cd42650af45897b89d6002c9e07739f263
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-05-16 14:24:58 -0400

    remove old server code

--- src/server/express/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-/*
-  Each named Router object in this directory should be exported from here,
-  to be added to the express app in the parent express/index file. 
- */
-
-export { AppRouter } from "./app";
-export { UsersRouter } from "./users";
