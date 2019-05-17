615a457ccc2a10798f89a89f71601635d1369aa3
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-05-17 16:30:29 -0400

    set up mongoose connection

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,20 +1,38 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './controllers';
 import { AppService } from './services';
 import { devServer, hotServer } from './middleware';
 
-const { NODE_ENV } = process.env;
+const {
+  NODE_ENV,
+  DB_HOSTNAME,
+  DB_PORT,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+} = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}`,
+      {
+        useNewUrlParser: true,
+        user: DB_USERNAME,
+        pass: DB_PASSWORD,
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
+class AppModule implements NestModule {
+  public configure(consumer: MiddlewareConsumer): void {
     if (NODE_ENV === 'development') {
       consumer.apply(devServer, hotServer).forRoutes('/');
     }
   }
-
 }
+
+export { AppModule };
