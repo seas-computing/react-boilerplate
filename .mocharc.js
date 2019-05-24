4820301ea9ee2f4c9ae4b933a5793961ab5dd255
Jonathan Seitz <jseitz@seas.harvard.edu>
2019-05-24 15:18:53 -0400

    Update testing configs
    Makes sure that mocha will load all ts(x) test files, and disables the
    explicit return type rule within those files (so we don't have to add
    ": void" after all of our `describe` callbacks).
    
    Also provides better coverage reports when running in non-watch mode via
    nyc

--- .mocharc.js
+++ .mocharc.js
@@ -1,22 +1,26 @@
 let specGlobs = [];
 if (process.argv.includes('--client')) {
   specGlobs.push('src/client/**/__tests__/*.test.ts');
+  specGlobs.push('src/client/**/__tests__/*.test.tsx');
 }
 if (process.argv.includes('--server')) {
   specGlobs.push('src/server/**/__tests__/*.test.ts');
+  specGlobs.push('src/server/**/__tests__/*.test.tsx');
 }
 module.exports = {
   exit: true,
   ui: 'bdd',
   reporter: 'spec',
   timeout: 30000,
-  file: '.mochainit.js',
+  file: '.mochainit.ts',
   recursive: true,
   require: [
     'jsdom-global/register',
     'raf/polyfill',
-    'ts-node',
+    'ts-node/register',
   ],
-  extension: ['ts'],
-  spec: specGlobs.length > 0 ? specGlobs : 'src/**/*.test.ts',
+  extension: ['ts', 'tsx', 'js'],
+  spec: specGlobs.length > 0
+    ? specGlobs
+    : ['src/**/*.test.ts', 'src/**/*/*.test.tsx'],
 }
