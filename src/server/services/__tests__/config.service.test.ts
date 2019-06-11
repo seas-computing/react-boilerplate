a3f568b9e98d9b3c7b1cfba370202fe52e0d3360
Robert Main <rmain@seas.harvard.edu>
2019-06-11 12:01:34 -0400

    Config service tests
    Add tests to cover behaviour in configuration service and increase the global app coverage level to >=80%

---
+++ src/server/services/__tests__/config.service.test.ts
@@ -0,0 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { strictEqual } from 'assert';
+import { stub } from 'sinon';
+import { ConfigService } from '../config.service';
+import { safeString } from 'testData';
+
+describe('Configuration Service', function () {
+  it('reports if the app is in production', async function () {
+    process.env.NODE_ENV = 'production';
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ConfigService,
+      ],
+    }).compile();
+
+    const config = module.get<ConfigService>(ConfigService);
+
+    strictEqual(config.isProduction, true);
+    strictEqual(config.isDevelopment, false);
+  });
+
+  it('reports if the app is in development', async function () {
+    process.env.NODE_ENV = 'development';
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ConfigService,
+      ],
+    }).compile();
+
+    const config = module.get<ConfigService>(ConfigService);
+
+    strictEqual(config.isProduction, false);
+    strictEqual(config.isDevelopment, true);
+  });
+
+  it('provides access to arbitary environment variables', async function () {
+    process.env.DB_HOSTNAME = safeString;
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ConfigService,
+      ],
+    }).compile();
+
+    const config = module.get<ConfigService>(ConfigService);
+
+    strictEqual(config.get('DB_HOSTNAME'), safeString);
+  });
+});
