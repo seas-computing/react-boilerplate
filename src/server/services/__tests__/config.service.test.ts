6b4f661666db8bd557d1df85a9e04386cbcf843a
Robert Main <rmain@seas.harvard.edu>
2019-06-13 17:35:55 -0400

    Add test for mongooseOptions getter

--- src/server/services/__tests__/config.service.test.ts
+++ src/server/services/__tests__/config.service.test.ts
@@ -1,9 +1,10 @@
 import { strictEqual } from 'assert';
-import { safeString } from 'testData';
+import { safeString, int } from 'testData';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
 import { ConfigService } from '../config.service';
 
 describe('Configuration Service', function () {
-  it('reports if the app is in production', async function () {
+  it('reports if the app is in production', function () {
     const config = new ConfigService({
       NODE_ENV: 'production',
     });
@@ -12,7 +13,7 @@ describe('Configuration Service', function () {
     strictEqual(config.isDevelopment, false);
   });
 
-  it('reports if the app is in development', async function () {
+  it('reports if the app is in development', function () {
     const config = new ConfigService({
       NODE_ENV: 'development',
     });
@@ -21,11 +22,47 @@ describe('Configuration Service', function () {
     strictEqual(config.isDevelopment, true);
   });
 
-  it('provides access to arbitary environment variables', async function () {
+  it('provides access to arbitary environment variables', function () {
     const config = new ConfigService({
       DB_HOSTNAME: safeString,
     });
 
     strictEqual(config.get('DB_HOSTNAME'), safeString);
+  });
+
+  describe('mongoose options', function () {
+    const DB_HOSTNAME = 'hostname';
+    const DB_PORT = int.toString();
+    const DB_DATABASE = 'database';
+    const DB_USERNAME = 'username';
+    const DB_PASSWORD = 'password';
+
+    let mongooseOptions: MongooseModuleOptions;
+
+    beforeEach(function () {
+      const config = new ConfigService({
+        DB_HOSTNAME,
+        DB_PORT,
+        DB_DATABASE,
+        DB_USERNAME,
+        DB_PASSWORD,
+      });
+      ({ mongooseOptions } = config);
+    });
+
+    it('creates a URI connection string from environment vars', function () {
+      strictEqual(
+        mongooseOptions.uri,
+        `mongodb://${DB_HOSTNAME}:${DB_PORT}/${DB_DATABASE}`
+      );
+    });
+
+    it('provides the mongo username', function () {
+      strictEqual(mongooseOptions.user, DB_USERNAME);
+    });
+
+    it('provides the mongo password', function () {
+      strictEqual(mongooseOptions.pass, DB_PASSWORD);
+    });
   });
 });
