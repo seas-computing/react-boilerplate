2560bd5a21a3fb05b10af708a4173494bbf11607
Robert Main <rmain@seas.harvard.edu>
2019-06-27 11:09:42 -0400

    Merge `rawRegularUserData` and `regularUser`
    The former was only used as a way of separating out the user data from the user object instantiation as is no longer required

--- src/common/__tests__/data/users.ts
+++ src/common/__tests__/data/users.ts
@@ -1,10 +1,8 @@
 import { User } from 'server/entities';
 
-export const rawRegularUserData: User = {
+export const regularUser: User = {
   firstName: 'Regular',
   lastName: 'User',
   id: '84ac7431-f612-49c4-953c-0497f44cebc2',
   email: 'test@seas.harvard.edu',
 };
-
-export const regularUser = rawRegularUserData;
